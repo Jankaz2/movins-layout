27f5f035de3caf9bffd80aea7f338fd0f0e581ff
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-05-16 22:43:48 +0200

    Modify input fields in contact section

--- src/contact_section/contact.js
+++ src/contact_section/contact.js
@@ -9,11 +9,11 @@ class Contact extends Component {
                 <div className="contact-form">
                     <h3 className="font-link">Contact us</h3>
                     <label htmlFor="name">First name</label>
-                    <input type="text" id='name' name='firstname' placeholder='First name'/>
+                    <input type="text" className='no-outline' id='name' name='firstname' placeholder='First name'/>
                     <label htmlFor="name">Surname</label>
-                    <input type="text" id='surname' name='lastname' placeholder='Surname'/>
+                    <input type="text" className='no-outline' id='surname' name='lastname' placeholder='Surname'/>
                     <label htmlFor="name">Email</label>
-                    <input type="email" id='name' name='userEmail' placeholder='e-mail'/>
+                    <input type="email" className='no-outline' id='name' name='userEmail' placeholder='e-mail'/>
                     <label htmlFor="textarea">Message</label>
                     <textarea name="textarea" id="textarea" cols="30" rows="10"
                               placeholder='Leave us message'></textarea>
