a806bc70849fc52614b4b3e32ed6d96719ed3614
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-05-17 10:08:20 +0200

    Create footer section

---
+++ src/footer/footer.js
@@ -0,0 +1,57 @@
+import React, {Component} from "react"
+import FooterCss from "./footer.css"
+import {FaTwitter, FaFacebook, FaInstagram, FaGoogle}
+    from "react-icons/fa"
+
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <div className="row font-link">
+                    <h2>Socials</h2>
+                    <div className={"icon twitter-icon"}><FaTwitter/></div>
+                    <div className={"icon facebook-icon"}><FaFacebook/></div>
+                    <div className={"icon instagram-icon"}><FaInstagram/></div>
+                    <div className={"icon google-icon"}><FaGoogle/></div>
+                </div>
+                <div className="footer-info">
+                    <div className="row">
+                        <div className="col span-1-of-3">
+                            <ul>
+                                <li><h4>Cinema</h4></li>
+                                <li>Privacy Politic</li>
+                                <li>Cookies</li>
+                                <li>Support</li>
+                            </ul>
+                        </div>
+                        <div className="col span-1-of-3">
+                            <ul>
+                                <li><h4>Company</h4></li>
+                                <li>About</li>
+                                <li>Partners</li>
+                                <li>Support</li>
+                                <li>FAQ</li>
+                            </ul>
+                        </div>
+                        <div className="col span-1-of-3">
+                            <ul className='footer-contacts'>
+                                <li><h4>Contacts</h4></li>
+                                <li>Cinema, Inc</li>
+                                <li>225 View Street</li>
+                                <li>San Diego, USA</li>
+                                <li>Tel: <span className="footer-number">+99 111 222 333</span></li>
+                                <li>E-mail: <span className="footer-email">cinema@cinema.com</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="copyright-info">
+                        <p>Copyright &copy; 2021 Cinema. All rights reserved. </p>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import StepsSection from "./steps_section/stepsSection";
 import AboutUsSection from "./about_us_section/aboutUsSection";
 import Contact from "./contact_section/contact";
 import Reviews from "./reviews_section/reviews";
+import Footer from "./footer/footer"
 
 ReactDOM.render(
     <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.render(
         <AboutUsSection/>
         <Reviews/>
         <Contact/>
+        <Footer/>
     </React.StrictMode>,
     document.getElementById('root')
 );
