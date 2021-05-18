0dca9549428336aeb1429675d03db24d746a0a6e
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-05-18 16:27:54 +0200

    Create login and registry boxes, modify footer, create email sender

--- src/contact_section/contact.js
+++ src/contact_section/contact.js
@@ -1,29 +1,53 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {useForm} from "react-hook-form"
 import ContactCss from './contact.css'
 import GridCss from "../grid/grid.css"
 
-class Contact extends Component {
-    render() {
-        return (
-            <section className='contact-us-section'>
-                <div className="contact-form">
-                    <h3 className="font-link">Contact us</h3>
-                    <label htmlFor="name">First name</label>
-                    <input type="text" className='no-outline' id='name' name='firstname' placeholder='First name'/>
-                    <label htmlFor="name">Surname</label>
-                    <input type="text" className='no-outline' id='surname' name='lastname' placeholder='Surname'/>
-                    <label htmlFor="name">Email</label>
-                    <input type="email" className='no-outline' id='name' name='userEmail' placeholder='e-mail' required={true}/>
-                    <label htmlFor="name">Subject</label>
-                    <input type="email" className='no-outline' id='subject' name='subject' placeholder='Subject' required={true}/>
-                    <label htmlFor="textarea">Message</label>
-                    <textarea name="textarea" id="textarea" cols="30" rows="10"
-                              placeholder='Leave us message'></textarea>
-                    <input type="submit" className='send-message-btn' value="Send message"/>
-                </div>
-            </section>
-        );
-    }
-}
+export default function Contact() {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const onSubmit = data => {
+        const email = data.email
+        const sub = data.subject
+        const content = data.content
 
-export default Contact
+        fetch(`http://localhost:4000/cinema/send-mail?from=${email}&subject=${sub}&text=${content}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
+    return (
+        <section className='contact-us-section'>
+            <div className="contact-form">
+                <h3 className="font-link">Contact us</h3>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label htmlFor="name">First name</label>
+                    <input type="text" className='no-outline' placeholder='First name'
+                           name={"name"}
+                           {...register("name", { required: true })}
+                    />
+                    <label htmlFor="surname">Surname</label>
+                    <input type="text" className='no-outline' placeholder='Surname'
+                           name={"surname"}
+                           {...register("surname", { required: true })}
+                    />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className='no-outline' placeholder='e-mail'
+                           name={"email"}
+                           {...register("email", { required: true })}
+                    />
+                    <label htmlFor="subject">Subject</label>
+                    <input type="text" className='no-outline' placeholder='Subject'
+                           name={"subject"}
+                           {...register("subject", { required: true })}
+                    />
+                    <label htmlFor="content">Message</label>
+                    <textarea name="content" cols="30" rows="10"
+                              placeholder='Leave us message'
+                              {...register("content", { required: true })}
+                    />
+                    <input type="submit" className='send-message-btn'/>
+                </form>
+            </div>
+        </section>
+    );
+}
--- src/navigation/navbar.js
+++ src/navigation/navbar.js
@@ -1,60 +1,94 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
 import NavbarCss from './navbar.css'
+import {Link} from "react-scroll"
 
-class Navbar extends Component {
-    listener = null
-    state = {
-        nav: false
-    }
+function Navbar() {
+    const [scroll, setScroll] = useState(false)
+    const [showLogin, setShowLogin] = useState(false)
+    const [showRegistry, setShowRegistry] = useState(false)
 
-    componentDidMount() {
-        window.addEventListener("scroll", this.handleScroll);
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll');
-    }
-
-    handleScroll = () => {
+    const handleScroll = () => {
         if (window.pageYOffset > 90) {
-            if (!this.state.nav) {
-                this.setState({nav: true});
-            }
+            setScroll(true)
         } else {
-            if (this.state.nav) {
-                this.setState({nav: false});
-            }
+            setScroll(false)
         }
     }
 
-    render() {
-        return (
-            <header>
-                <nav>
-                    <ul className={`navbar ${this.state.nav && 'sticky'}`}>
-                        <ul className="navbar-options">
-                            <li className="navbarItem font-link"><a href="">Show films</a></li>
-                            <li className="navbarItem font-link"><a href="">Buy ticket</a></li>
-                            <li className="navbarItem font-link"><a href="">Find cinema</a></li>
-                        </ul>
-                        <ul className="navbar-info">
-                            <li className="navbarItem font-link"><a href="">Contact</a></li>
-                            <li className="navbarItem font-link"><a href="">About us</a></li>
-                            <li className="navbarItem font-link"><a href="">Log in</a></li>
-                            <li className="navbarItem font-link"><a href="">Sign up</a></li>
-                        </ul>
-                        <span><div className="logo"><a href="#">cinema</a></div></span>
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+    })
+
+    return (
+        <header>
+            <nav>
+                <ul className={`navbar ${scroll && 'sticky'}`}>
+                    <ul className="navbar-options">
+                        <li className="navbarItem font-link"><a href="">Show films</a></li>
+                        <li className="navbarItem font-link"><a href="">Buy ticket</a></li>
+                        <li className="navbarItem font-link"><a href="">Find cinema</a></li>
                     </ul>
-                </nav>
-                <div className="webstart-page">
-                    <h2>Cinema is not only about making people dream.
-                        <br/>&nbsp;&nbsp;&nbsp;It's about changing things and making people think.
-                    </h2>
+                    <ul className="navbar-info">
+                        <li className="navbarItem font-link"><Link activeClass="active" to="contact-us-section"
+                                                                   spy={true} smooth={true} offset={-60}
+                                                                   duration={500}>
+                            <a href="">Contact</a></Link></li>
+                        <li className="navbarItem font-link"><Link to="about-us-section" spy={true} smooth={true}
+                                                                   offset={-60} duration={500}>
+                            <a href="">About us</a></Link></li>
+                        <li className="navbarItem font-link log-in-link"><a href="" onClick={(event) => {
+                            setShowLogin(!showLogin)
+                            if (showRegistry) {
+                                setShowRegistry(!showRegistry)
+                            }
+                            event.preventDefault()
+                        }}>Log in</a></li>
+                        <li className="navbarItem font-link sign-up-link"><a href="" onClick={(event) => {
+                            setShowRegistry(!showRegistry)
+                            if (showLogin) {
+                                setShowLogin(!showLogin)
+                            }
+                            event.preventDefault()
+                        }}>Sign up</a></li>
+                    </ul>
+                    <span><div className="logo"><a href="#">cinema</a></div></span>
+                </ul>
+            </nav>
+            {
+                showLogin &&
+                <div className="login-box">
+                    <form>
+                        <input type="text" placeholder="login"/>
+                        <input type="password" placeholder="password"/>
+                        <input type="submit" value="Log in"/>
+                    </form>
                 </div>
-                <button className="show-more">Show more</button>
-            </header>
-        )
-    }
+            }
+            {
+                showRegistry &&
+                <div className="signup-box">
+                    <form>
+                        <input type="name" placeholder="Name"/>
+                        <input type="text" placeholder="Surname"/>
+                        <input type="email" placeholder="e-mail"/>
+                        <input type="number" placeholder="phone number"/>
+                        <input type="password" placeholder="password"/>
+                        <input type="password" placeholder="repeat password"/>
+                        <input type="submit" value="Sign in"/>
+                    </form>
+                </div>
+            }
+            <div className="webstart-page">
+                <h2>Cinema is not only about making people dream.
+                    <br/>&nbsp;&nbsp;&nbsp;It's about changing things and making people think.
+                </h2>
+            </div>
+            <button className="show-more"><Link activeClass="active" to="steps-section" spy={true} smooth={true}
+                                                offset={-60} duration={500}>
+                <a href="">Show more</a>
+            </Link></button>
+        </header>
+    )
 }
 
 export default Navbar
