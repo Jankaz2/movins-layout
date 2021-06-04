e64d008aedfabac64cc8553a29670aa2a370d4db
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-06-04 23:06:24 +0200

    Modify aboutUsSection css syntax to scss

--- src/about_us_section/aboutUsSection.js
+++ src/about_us_section/aboutUsSection.js
@@ -9,25 +9,25 @@ class AboutUsSection extends Component {
         return (
             <section className='about-us-section'>
                 <div className="u-center-text">
-                    <h2 className='about-us-section-text'>The name of culture is cinemafactory</h2>
+                    <h2 className='heading-secondary'>The name of culture is cinemafactory</h2>
                 </div>
                 <div className='row'>
-                    <div className='col span-1-of-2 box about-us-section-text'>
-                        <h3>Who we are?</h3>
+                    <div className='col span-1-of-2 box'>
+                        <h3 className='heading-tertiary'>Who we are?</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             Adipisci corporis cumque delectus deleniti earum explicabo
                             fuga laudantium odit porro similique.
                         </p><br/>
-                        <h3>Find your dreams</h3>
+                        <h3 className='heading-tertiary'>Find your dreams</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                             Adipisci corporis cumque delectus deleniti earum explicabo
                         </p>
                     </div>
                     <div className='col span-1-of-2 box'>
                         <div className="composition">
-                            <img src={img1} alt="Picture 1" className='photo photo-1'/>
-                            <img src={img2} alt="Picture 2" className='photo photo-2'/>
-                            <img src={img3} alt="Picture 3" className='photo photo-3'/>
+                            <img src={img1} alt="Picture 1" className='composition__photo composition__photo-1'/>
+                            <img src={img2} alt="Picture 2" className='composition__photo composition__photo-2'/>
+                            <img src={img3} alt="Picture 3" className='composition__photo composition__photo-3'/>
                         </div>
                     </div>
                 </div>
