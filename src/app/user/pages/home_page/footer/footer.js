372d18e9b0914e6974eccf60f2adc9951612ada5
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-07-27 09:50:47 +0200

    Modify footer layout

--- src/app/user/pages/home_page/footer/footer.js
+++ src/app/user/pages/home_page/footer/footer.js
@@ -1,49 +1,46 @@
 import React, {Component} from "react"
 import FooterCss from "./styles/footer.scss"
-import {FaTwitter, FaFacebook, FaInstagram, FaGoogle}
-    from "react-icons/fa"
-
 
 class Footer extends Component {
     render() {
         return (
-            <footer>
-                <div className="footer-info">
-                    <div className="row">
-                        <div className="col span-1-of-3">
-                            <ul>
-                                <li><h4>Cinema</h4></li>
-                                <li>Privacy Politic</li>
-                                <li>Cookies</li>
-                                <li>Support</li>
-                            </ul>
-                        </div>
-                        <div className="col span-1-of-3">
-                            <ul>
-                                <li><h4>Company</h4></li>
-                                <li>About</li>
-                                <li>Partners</li>
-                                <li>Support</li>
-                                <li>FAQ</li>
-                            </ul>
-                        </div>
-                        <div className="col span-1-of-3">
-                            <ul className='footer-contacts'>
-                                <li><h4>Contacts</h4></li>
-                                <li>Cinema, Inc</li>
-                                <li>225 View Street</li>
-                                <li>San Diego, USA</li>
-                                <li>Tel: <span className="footer-number">+99 111 222 333</span></li>
-                                <li>E-mail: <span className="footer-email">appcinema2021@gmail.com</span></li>
+            <footer className="footer">
+                <div className="footer__logo-box">
+                    <h1 className='heading--primary--main'>MOVINS</h1>
+                </div>
+                <div className="row">
+                    <div className="col span-1-of-2">
+                        <div className="footer__navigation">
+                            <ul className="footer__list">
+                                <li className="footer__item">
+                                    <a href="" className="footer__link">Company</a>
+                                </li>
+                                <li className="footer__item">
+                                    <a href="" className="footer__link">Carrer</a>
+                                </li>
+                                <li className="footer__item">
+                                    <a href="" className="footer__link">Privacy</a>
+                                </li>
+                                <li className="footer__item">
+                                    <a href="" className="footer__link">Terms</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="copyright-info">
-                        <p>Copyright &copy; 2021 Cinema. All rights reserved. </p>
+                    <div className="col span-1-of-2">
+                        <p className="footer_copyright">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Aliquam architecto culpa dolore dolorem doloremque dolores
+                            explicabo ipsam mollitia nobis pariatur qui quia, quibusdam
+                            quo quod tempora velit voluptatibus. Ad aliquam aut dicta,
+                            dolores eum illum impedit iusto numquam optio possimus quae
+                            sapiente tenetur unde ut vel. Consectetur delectus quasi tempora.
+                        </p>
                     </div>
                 </div>
             </footer>
-        );
+
+    );
     }
 }
 
