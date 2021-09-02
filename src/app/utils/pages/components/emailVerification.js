7b6250ef45ff6fefb3f0df9af416079d39208d84
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-09-02 21:22:02 +0200

    Modify style

--- src/app/utils/pages/components/emailVerification.js
+++ src/app/utils/pages/components/emailVerification.js
@@ -5,7 +5,7 @@ import EmailVerificationSCSS from "../styles/emailVerification.scss"
 const EmailVerification = () => {
     return (
         <div>
-            <div className='user-activation'>
+            <div className='email-verification'>
                 <div className='correct-200'>
                     <h1 className='correct-200__text'>Check your email to verify account</h1>
                     <span className='email-verification__check-icon'><FaRegEnvelope/></span>
