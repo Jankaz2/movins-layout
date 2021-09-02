b57d65de11dae30e9e1944270afbfa308c37abd2
Jankaz2 <jan.kazmierczak.2001@wp.pl>
2021-09-02 21:01:52 +0200

    Delete unused page

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import {Route, Switch} from "react-router-dom";
 import HomePage from "./user/pages/home_page/header/components/homePage";
 import Footer from "./user/pages/home_page/footer/components/footer";
@@ -7,7 +7,6 @@ import Cinema from "./user/pages/single_cinema_page/cinema/components/cinema"
 import AdminPage from "./admin/pages/admin_page/adminPage";
 import PageNotFound from "./utils/pages/components/pageNotFound";
 import MyAccount from "./user/pages/user_account/components/myAccount";
-import UserActivation from "./utils/pages/components/userActivation";
 import EmailVerification from "./utils/pages/components/emailVerification";
 import Main from "./utils/pages/main";
 
@@ -17,7 +16,6 @@ function App() {
             <Switch>
                 <Route path="/cinema-list" exact component={CinemasList}/>
                 <Route path="/cinema-list/cinema/:id" exact component={Cinema}/>
-                <Route path="/users/activation" component={UserActivation}/>
                 <Route path="/users/verification" component={EmailVerification}/>
                 <Route path="/" exact component={HomePage}/>
                 <Route path="/admin" exact component={AdminPage}/>
